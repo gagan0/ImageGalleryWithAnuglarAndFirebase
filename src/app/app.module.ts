7f16cf316b4d50c4c943dffb9312ab65f940265e
Gagan (Mac) <gagan.maheshwari@trakkerz.com>
2019-07-16 19:21:15 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
