309136f4775690f2c89fc0b784926cecea2b5d9d
Gagan (Mac) <gagan.maheshwari@trakkerz.com>
2019-08-06 20:12:59 +0530

    Final changes

--- src/app/images/image-list/image-list.component.ts
+++ src/app/images/image-list/image-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ImageService } from 'src/app/shared/image.service';
 
 @Component({
-  selector: 'app-image-list',
-  templateUrl: './image-list.component.html',
-  styles: []
+	selector: 'app-image-list',
+	templateUrl: './image-list.component.html',
+	styles: []
 })
-export class ImageListComponent implements OnInit {
+export class ImageListComponent implements OnInit
+{
+	imageList: any[];
+	rowIndexArray: any[];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(private imageService: ImageService) { }
+	
+	ngOnInit()
+	{
+		this.imageService.imageDetailList.snapshotChanges().subscribe(
+			list =>
+			{
+				this.imageList = list.map(item =>
+					{
+						return item.payload.val();
+					});
+				
+				this.rowIndexArray = Array.from(Array(Math.ceil(this.imageList.length / 3)).keys());
+			}
+		)
+	}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+	production: true,
+	firebaseConfig:
+	{
+		apiKey: "AIzaSyCZg7lmneJ-3agReM_ElU6XGf1qqzmXFNM",
+		authDomain: "angular-image-gallery-900db.firebaseapp.com",
+		databaseURL: "https://angular-image-gallery-900db.firebaseio.com",
+		projectId: "angular-image-gallery-900db",
+		storageBucket: "angular-image-gallery-900db.appspot.com",
+		messagingSenderId: "849966186513",
+		appId: "1:849966186513:web:4b1ca2bce352611c"
+	}
 };
